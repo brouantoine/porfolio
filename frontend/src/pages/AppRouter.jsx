fc1a9c8b1691c558b7135ba28ed39eeb3c8a9c08
brouantoine <tutosplusconfiguration@gmail.com>
2025-08-21 21:15:41 +0000

    vercel

--- frontend/src/pages/AppRouter.jsx
+++ frontend/src/pages/AppRouter.jsx
@@ -4,17 +4,30 @@ import CV from './CV';
 import Projets from './Projets';
 import Contact from './Contact';
 import { HelmetProvider } from 'react-helmet-async';
-export default function AppRouter() {
+ function AppRouter() {
   return (
     <HelmetProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cv" element={<CV />} />
-          <Route path="/projets" element={<Projets />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </Router>
-    </HelmetProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cv" element={<CV />} />
+        <Route path="/projets" element={<Projets />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+        </HelmetProvider>
   );
 }
+
+export default AppRouter;
+
+
+
+
+
+
+
+
+
+
+
